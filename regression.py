406d0fa38ad5619583493c994e04437a9e07f1a8
devesh2002yadav <77203935+devesh2002yadav@users.noreply.github.com>
2023-01-27 18:04:54 +0530

    Add files via upload

---
+++ regression.py
@@ -0,0 +1,8 @@
+setChartLibrary('google-chart');
+setChartTitle('Polynomial Regression-LDR');
+setChartType('predictionGraph');
+setAxisName('Time','Light Intensity');
+mul(1);
+setAnimation(true);
+setCrosshair(true);
+plotChart('time_stamp','sens');
